05c57b2fb9502c89ca7b6a3487d68a441851cd36
ThePunisherOT <punisher_caner@windowslive.com>
2017-08-18 12:34:28 +0000

    Update stockrom

--- stockrom/system/bin/install-recovery.sh
+++ stockrom/system/bin/install-recovery.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 34238464 4dcc9717d1c7a1a7bb5a8e457ba8131f19ad1462 31723520 7534b167c7ee19923f92d4d79b4e070d617c7715
+  exec sh /system/etc/recovery-transform.sh 34238464 78879fe11a7ecc685963bd9d4389837f379f5545 31723520 26593d99bc1042d772d6f8dcf3da6a54b956a8d7
 fi
 
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:34238464:4dcc9717d1c7a1a7bb5a8e457ba8131f19ad1462; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:31723520:7534b167c7ee19923f92d4d79b4e070d617c7715 EMMC:/dev/block/bootdevice/by-name/recovery 4dcc9717d1c7a1a7bb5a8e457ba8131f19ad1462 34238464 7534b167c7ee19923f92d4d79b4e070d617c7715:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:34238464:78879fe11a7ecc685963bd9d4389837f379f5545; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:31723520:26593d99bc1042d772d6f8dcf3da6a54b956a8d7 EMMC:/dev/block/bootdevice/by-name/recovery 78879fe11a7ecc685963bd9d4389837f379f5545 34238464 26593d99bc1042d772d6f8dcf3da6a54b956a8d7:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
